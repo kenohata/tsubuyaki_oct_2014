864b37194a3a6914dd5c35f81bed9d33809c6d58
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 22:04:02 +0900

    :lipstick:

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :tweets
-
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 
   resources :users, only: [:index, :show]
+  resources :tweets
 
   root to: 'sessions#new'
 
