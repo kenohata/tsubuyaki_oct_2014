1c446ac3b7a810642a326dbf823aefc2fd7724d8
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 21:19:51 +0900

    define registrations#create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,20 @@
 class RegistrationsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(params_registration)
+
+    if @user.save
+      redirect_to root_path, notice: "you signed up!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_registration
+    params.permit(:email, :password)
+  end
 end
