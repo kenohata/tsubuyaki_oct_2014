4de0b5d4f385c2aff6492df50dea77cc259917c7
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 22:05:48 +0900

    assoc User and Tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_many :tweets, dependent: :destroy
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
