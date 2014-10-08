ee61534c7b89921b7350432fe5deefffdb154fa0
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 21:37:12 +0900

    define sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    email    = params_session[:email]
+    password = params_session[:password]
+
+    if @user = login(email, password)
+      redirect_to root_path, notice: "you signed in!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_session
+    params.permit(:email, :password)
+  end
 end
