b8a39ee68e2a11ce60dc9f59f6f5a3c772ea24c9
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 21:28:02 +0900

    define routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
   resource :registrations, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
