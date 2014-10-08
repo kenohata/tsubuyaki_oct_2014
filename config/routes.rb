728204afd22ce3b38b1fb6f3a0501b3d3464bc64
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 21:27:29 +0900

    rails g controller sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resource :registrations, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
