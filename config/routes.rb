68bf19028c5dedbd4629bad0168ba755d46f022a
Ken Ohata <ohata@daraf.co.jp>
2014-10-08 21:50:54 +0900

    rails g controller users index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/show'
+
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 
