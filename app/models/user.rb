58e0a72e0c65dfaa439bb806de5543cb26bb0865
Ken Ohata <ohata@daraf.co.jp>
2014-10-09 21:35:44 +0900

    assoc Favorite to User and Tweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
