28a810a6cd41e163bfb4f37996c458effd2914e6
Ken Ohata <ohata@daraf.co.jp>
2014-10-10 02:20:35 +0900

    assoc Follow and User

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,7 @@
 class Follow < ActiveRecord::Base
+  belongs_to :follower, class_name: User
+  belongs_to :inverse_follower, class_name: User
+
+  validates :follower, presence: true
+  validates :inverse_follower, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :follows, foreign_key: :inverse_follower_id
+  has_many :followers, through: :follows
+  has_many :inverse_follows, foreign_key: :follower_id
+  has_many :inverse_followers, through: :inverse_follows
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
