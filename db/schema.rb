296967a83e8279c57fa1cf2d8526f32010a9972f
Ken Ohata <ohata@daraf.co.jp>
2014-10-09 21:42:00 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008130154) do
+ActiveRecord::Schema.define(version: 20141009123340) do
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: true do |t|
     t.integer  "user_id"
