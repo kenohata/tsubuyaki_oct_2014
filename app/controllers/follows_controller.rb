b562117ea7584dcd564c2a248f0ae973e4c6c86f
Ken Ohata <ohata@daraf.co.jp>
2014-10-10 02:35:37 +0900

    update follows

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,18 @@
 class FollowsController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @user   = User.find(params[:user_id])
+    @user.followers << current_user
+
+    redirect_to tweets_url
+  end
+
+  def destroy
+    @user   = User.find(params[:user_id])
+    @follow = @user.follows.find_by!(follower_id: current_user.id)
+    @follow.destroy
+
+    redirect_to tweets_url
+  end
 end
