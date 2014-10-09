3238464c52b66ee89f4e84b7ef3c27492a7f43a8
Ken Ohata <ohata@daraf.co.jp>
2014-10-09 21:45:29 +0900

    update favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,21 @@
 class FavoritesController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @tweet    = Tweet.find(params[:tweet_id])
+    @favorite = @tweet.favorites.build(user: current_user)
+
+    if @favorite.save
+      redirect_to tweets_url, notice: "you favorited!"
+    else
+      redirect_to tweets_url, notice: "you've alrady favorited!"
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @favorite = @tweet.favorites.find_by!(user_id: current_user.id)
+    @favorite.destroy
+    redirect_to tweets_url, notice: "you unfavorited!"
+  end
 end
